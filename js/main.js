a12f210c3823700d6ce8f4dd4f527b6a93c244b0
Jesse Shawl <jesseshawl@gmail.com>
2013-03-11 14:00:28 -0400

    added logo

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ $('.js-toggle-menu').click(function(e){
 	e.preventDefault();
 	$(this).siblings().toggle();
 });
+
+$('.main-nav li').hover(function(){
+	//$(this).find('ul').toggleClass('active');
+});
+$('.main-nav li').click(function(){
+	$(this).find('ul').toggleClass('active');
+});
