53758f4c2aceac4e633217794844f0d15dd2243c
Jesse Shawl <jesseshawl@gmail.com>
2013-03-13 10:23:55 -0400

    supa clean css

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ $('.js-toggle-menu').click(function(e){
 	e.preventDefault();
 	$(this).siblings().toggle();
 });
-
-$('.main-nav li').hover(function(){
-	//$(this).find('ul').toggleClass('active');
-});
-$('.main-nav li').click(function(){
+$('.nav--primary li').click(function(){
 	$(this).find('ul').toggleClass('active');
 });
